1a44fbae994f6548271a5264e2c670eedb2b765d
nyook941 <73042201+nyook941@users.noreply.github.com>
2025-02-12 16:16:11 -0600

    add auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,34 +5,46 @@ import { NextRequest, NextResponse } from "next/server";
 const publicRoutes = ["/auth/login", "/auth/signup", "/auth/forgot"];
 
 export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const isPublicRoute = publicRoutes.includes(path);
+  const { pathname } = req.nextUrl;
+  const isPublicRoute = publicRoutes.includes(pathname);
+
   try {
     const cookieStore = await cookies();
-    const idToken = cookieStore.get("id_token")?.value;
-    const refreshToken = cookieStore.get("refresh_token")?.value;
-    let user = null;
-    if (idToken) {
-      user = await authApi.validateJwt(idToken);
-    } else if (refreshToken) {
-      // TODO: get new id token
+    const idToken =
+      cookieStore.get("id_token")?.value ||
+      (await getIdTokenFromRefresh(cookieStore.get("refresh_token")?.value));
+
+    if (!idToken && !isPublicRoute) {
+      return redirectToLogin(req);
     }
+
+    if (isPublicRoute && idToken) {
+      return redirectToHome(req);
+    }
+
+    const user = idToken && (await authApi.validateJwt(idToken));
 
     if (!isPublicRoute && !user) {
-      console.log("user is not authenticated");
-      return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
-    }
-
-    if (isPublicRoute && user) {
-      console.log("user is authenticated");
-      return NextResponse.redirect(new URL("/", req.nextUrl));
+      return redirectToLogin(req);
     }
   } catch (error) {
-    console.log("an error occurred:", error);
+    console.log("An error occurred:", error);
   }
 
   return NextResponse.next();
 }
+
+const getIdTokenFromRefresh = async (refreshToken: string | undefined) => {
+  if (refreshToken) {
+    return await authApi.refreshJwt(refreshToken);
+  }
+  return null;
+};
+
+const redirectToLogin = (req: NextRequest) =>
+  NextResponse.redirect(new URL("/auth/login", req.nextUrl));
+const redirectToHome = (req: NextRequest) =>
+  NextResponse.redirect(new URL("/", req.nextUrl));
 
 export const config = {
   matcher: ["/((?!api|auth|_next/static|_next/image|favicon.ico|images/).*)"],
