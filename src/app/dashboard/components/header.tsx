945d603dae10b67d93c12fe1419d62250b4e1edb
Kennan Wu <kennan.k.wu@gmail.com>
2025-02-16 23:32:13 -0600

    add skeleton for dashboard UI

--- src/app/dashboard/components/header.tsx
+++ src/app/dashboard/components/header.tsx
@@ -1,7 +1,11 @@
-export default function DashboardHeader() {
+export default function DashboardHeader({
+  title,
+}: {
+  title: string | string[] | undefined;
+}) {
   return (
-    <div className="flex h-fit flex-grow py-4 px-10 border-b border-gray-300">
-      <h2 className="text-base font-semibold">Recents</h2>
+    <div className="h-fit py-4 px-10 border-b border-gray-300">
+      <h2 className="text-base font-semibold">{title}</h2>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import DashboardHeader from "./dashboard/components/header";
-import DashboardSidebar from "./dashboard/components/sidebar";
-
 export default function Home() {
   return (
     <div className="bg-white flex flex-row">
-      <DashboardSidebar />
-      <DashboardHeader />
+      you are not meant to see this page!!!
     </div>
   );
 }
