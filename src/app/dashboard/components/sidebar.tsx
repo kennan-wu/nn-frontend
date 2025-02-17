b0047c58b3f9909fae53cc4613113eddc4775171
Kennan Wu <kennan.k.wu@gmail.com>
2025-02-17 00:24:50 -0600

    add UI skeleton for dashboard

--- src/app/dashboard/components/sidebar.tsx
+++ src/app/dashboard/components/sidebar.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { FaCaretDown } from "react-icons/fa";
 import { HiOutlineTemplate } from "react-icons/hi";
-import { IoHelp, IoSettingsOutline } from "react-icons/io5";
-import { MdInfoOutline } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdInfoOutline, MdOutlineBallot } from "react-icons/md";
 import { VscFile, VscFiles } from "react-icons/vsc";
 
-export default function DashboardSidebar() {
+export default function DashboardSidebar({ selected }: { selected: string }) {
+  const router = useRouter();
+
+  const handleLocationClick = (slug: string) => {
+    router.push(`/dashboard/${slug}`);
+  };
+
   return (
     <div className="flex flex-col justify-between w-72 min-h-screen border-r border-gray-300">
       <div>
@@ -21,44 +28,62 @@ export default function DashboardSidebar() {
             <h2 className="text-base font-semibold">Kennan Wu</h2>
             <FaCaretDown />
           </button>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300">
+          <button
+            onClick={() => handleLocationClick("projects")}
+            disabled={selected === "projects"}
+            className={`flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 ${
+              selected === "projects" && "bg-gray-300"
+            }`}
+          >
             <VscFiles />
             Projects
           </button>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300">
+          <button
+            onClick={() => handleLocationClick("templates")}
+            disabled={selected === "templates"}
+            className={`flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 ${
+              selected === "templates" && "bg-gray-300"
+            }`}
+          >
             <HiOutlineTemplate />
             Templates
           </button>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300">
-            <IoHelp />
-            Help
+          <button
+            onClick={() => handleLocationClick("all")}
+            disabled={selected === "all"}
+            className={`flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 ${
+              selected === "all" && "bg-gray-300"
+            }`}
+          >
+            <MdOutlineBallot />
+            All
           </button>
         </div>
         <div className="py-3">
           <h2 className="px-5 text-base font-semibold">Recents</h2>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
+          <button className="flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
             <VscFile />
             Project Name
           </button>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
+          <button className="flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
             <VscFile />
             Project Name
           </button>
         </div>
         <div className="py-3">
           <h2 className="px-5 text-base font-semibold">Favorites</h2>
-          <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
+          <button className="flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
             <VscFile />
             Project Name
           </button>
         </div>
       </div>
       <div className="pb-3">
-        <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
+        <button className="flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
           <IoSettingsOutline />
           Settings
         </button>
-        <button className="flex flex-row justify-start items-center p2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
+        <button className="flex flex-row justify-start items-center p-2 gap-2 text-sm px-6 py-2 transition hover:bg-gray-300 w-full">
           <MdInfoOutline />
           Info
         </button>
