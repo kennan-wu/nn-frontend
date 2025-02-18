2eb583a36dc8c2d44809581e1d549f5fdd734a73
Kennan Wu <kennan.k.wu@gmail.com>
2025-02-18 13:56:38 -0600

    add extra stuff

--- src/app/dashboard/components/action-button.tsx
+++ src/app/dashboard/components/action-button.tsx
@@ -1,5 +1,4 @@
 import { IconType } from "react-icons";
-import { HiOutlineSparkles } from "react-icons/hi";
 
 export interface ActionButtonProps {
   actionName: string;
@@ -18,6 +17,7 @@ export default function DashboardActionButton({
 }: ActionButtonProps) {
   return (
     <button
+      onClick={action}
       className={`flex justify-start bg-gray-200 rounded px-5 py-9 text-sm font-semibold items-center gap-3 hover:${hoverColor} transition`}
     >
       <Icon className={`${iconColor} rounded-full w-9 h-9 p-2 text-white`} />
